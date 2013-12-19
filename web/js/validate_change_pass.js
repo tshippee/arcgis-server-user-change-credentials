0e4b9ce1006b7febb80bcd400f11d149da8587a2
Joel McCune <knu2xs@gmail.com>
2013-12-18 19:32:24 -0500

    Migrated entire form to a panel and have submission logic roughed in. The only problem is I cannot figure out how to keep the function from running at load. I just want it to be available when the form submits. Speaking of submitting the form. The post process, I have no freaking clue with this validation and jQuery posting. This is really freaking annoying. Also, the Python script REST endpoint quit working, so I need to troubleshoot that as well. I cannot even seem to get it running from the desktop. It may be I pulled the passwords out since posting it to GitHub, but I published it to the server before doing that part, so I have no clue. Oye, this is turning out to be a pain in the arse.

--- web/js/validate_change_pass.js
+++ web/js/validate_change_pass.js
@@ -1,8 +1,44 @@
 /**
  * Created by joel5174 on 12/17/13.
  */
+
+// submit handler for form
+function submit_form () {
+
+    // url of the rest endpoint created when publishing the Python tool
+    var restUrl = 'http://gis1:6080/arcgis/rest/services/ChangeUserPassword/GPServer/Change%20User%20Password/execute'
+
+    // extract data from form for posting in header
+    var username = $("input#username").val();
+    var password_old = $("input#old_password").val();
+    var password_new = $("input#new_password").val();
+    console.log('submit');
+
+    // if there, remove the alert-danger class
+    $("div.alert[class~='alert-danger']").removeClass('alert-danger');
+
+    // set alert div to alert-info
+    $("div.alert:not([class~='alert-info'])").addClass('alert-info');
+
+    // single find for div.alert
+    var $div_alert = $("div.alert");
+
+    // add progress bar and message to alert div
+    $div_alert.html(
+        '<div class="progress progress-striped active">\n    <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"\n         style="width: 100%">\n    </div>\n</div>\n<div>Submitting new credentials to server</div>\n'
+    );
+
+    // fade in the alert div
+    $div_alert.fadeIn();
+
+    // post to REST endpoint
+//    $.post(restUrl, )
+};
+
+// use jquery validate to configure form validation
 $("#update_username_form").validate({
-    debug: true,
+
+    // apply rules to fields, most are required and the confirm field must match the new password
     rules: {
         username: "required",
         old_password: "required",
@@ -11,35 +47,64 @@ $("#update_username_form").validate({
             equalTo: "#new_password"
         }
     },
+
+    // custom messages for each field if failing validation
     messages: {
         username: "please enter your username",
         old_password: "please enter your current password",
         new_password: "please enter your new password",
         new_password_confirm: "please ensure your new passwords match"
     },
+
+    // if the form fails validation
     invalidHandler: function (event, validator) {
+
+        // get and store the number of invalid fields
         var errors = validator.numberOfInvalids();
+
+        // if errors exist
         if (errors) {
+
+            // create a variable for messages
             var message = errors == 1
+
+                // if only one error, present this message
                 ? 'One field is invalid. Please correct the error and resubmit.'
+
+                // if multiple errors, present this message
                 : 'There are ' + errors + ' invalid fields. Please correct the errors and resubmit.';
+
+            // query the DOM for the alert div and save as a variable
             var $div_alert = $('div.alert');
+
+            // populate the error message in the alert div
             $div_alert.html(message);
+
+            // set class on alert box to alert-danger if it does not already exist
+            $("div.alert:not([class~='alert-danger'])").addClass('alert-danger');
+            $("div.panel:not([class~='panel-danger'])").addClass('panel-danger');
+
+            // fade in the alert div
             $div_alert.fadeIn();
+
         } else {
+
+            // if the form is valid, fade the alert div out if it is not already hidden
             $('div.alert').fadeOut();
+
+            // remove the alert-danger class
+            $("div.alert[class~='alert-danger']").removeClass('alert-danger');
+            $("div.panel[class~='panel-danger']").remoteClass('panel-danger');
         }
-    }
-});
-$('#change_password_submit').click( function () {
-    var restUrl = 'http://gis1:6080/arcgis/rest'
-    $.post(url, 
-        {
-            'username': $('#username').val(),
-            'old_pass': $('#old_password').val(), 
-            'new_pass': $('#new_password').val()
-        },
-        success(data, textStatus, jqXHR),
-        'dataType': 'json'
-    );
+    },
+
+    submitHandler: submit_form()
+
+    // tag fields as errors when invalid
+//    showErrors: function(errorMap, errorList) {
+//        for (var error in errorList) {
+//            console.log(error['element']);
+////            errorElement.element.parent('div.form-group').addClass('has-error');
+//        }
+//    }
 });
